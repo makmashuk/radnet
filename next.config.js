0e94d4ab5227b61e17a9c26398e5ad7e2a9ab323
Md. Mohiuddin Khan <mkhan@relisource.com>
2022-08-31 19:23:42 +0600

    Updated base and assetPrefix

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
-  basePath:'https://makmashuk.github.io/radnet/',
+  basePath:'makmashuk.github.io/radnet/',
   assetPrefix:'/'
 }
 
